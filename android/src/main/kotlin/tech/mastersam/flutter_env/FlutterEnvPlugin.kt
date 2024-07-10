f949ac7727d419f76bceb23c4f079c9c38b65fc8
Codefarmer <abadasamuelosp@gmail.com>
2024-07-10 14:02:06 +0100

    🔥 Chore: remove unneccessary method handler from plugin

--- android/src/main/kotlin/tech/mastersam/flutter_env/FlutterEnvPlugin.kt
+++ android/src/main/kotlin/tech/mastersam/flutter_env/FlutterEnvPlugin.kt
@@ -3,33 +3,11 @@ package tech.mastersam.flutter_env
 import androidx.annotation.NonNull
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
 
 /** FlutterEnvPlugin */
-class FlutterEnvPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class FlutterEnvPlugin: FlutterPlugin {
 
-  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_env")
-    channel.setMethodCallHandler(this)
-  }
+  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {}
 
-  override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
-    }
-  }
-
-  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {}
 }
