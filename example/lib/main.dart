fb36d801483d7277736f351c6fbc7e0d10984080
Codefarmer <abadasamuelosp@gmail.com>
2024-07-10 14:01:21 +0100

    🚧 Chore: add env to sample

---
+++ example/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String name = const String.fromEnvironment("APP_NAME");
+  String suffix = const String.fromEnvironment("APP_SUFFIX");
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('name: $name\n'),
+              Text('suffix: $suffix\n'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
